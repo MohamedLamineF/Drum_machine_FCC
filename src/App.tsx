35763ec3325bf4ae147a0a7ff42f9e0c8981ea57
MohamedLamineF <fadimoh@hotmail.fr>
2024-12-11 23:22:25 +0100

    prepare

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="relative h-screen">
         <div className="absolute inset-0">
-          <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] [&>div]:absolute [&>div]:inset-0 [&>div]:bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]">
+          <div className="absolute top-0 -z-10 h-full w-full bg-white [&>div]:absolute [&>div]:bottom-auto [&>div]:left-auto [&>div]:right-0 [&>div]:top-0 [&>div]:h-[500px] [&>div]:w-[500px] [&>div]:-translate-x-[30%] [&>div]:translate-y-[20%] [&>div]:rounded-full [&>div]:bg-[rgba(109,244,173,0.5)] [&>div]:opacity-50 [&>div]:blur-[80px]">
             <div></div>
           </div>
         </div>
@@ -13,8 +13,8 @@ function App() {
         <div className="relative z-10 flex h-full flex-col items-center justify-center px-4">
           <div className="max-w-3xl text-center">
             <h1 className="mb-8 text-4xl font-bold tracking-tight sm:text-6xl lg:text-7xl text-slate-900">
-              Markdown
-              <span className="text-sky-900"> Previewer</span>
+              Drum
+              <span className="text-sky-900"> Machine</span>
             </h1>
           </div>
         </div>
