6516132dbe74f7df6f4392896029012ea11cddc5
MohamedLamineF <fadimoh@hotmail.fr>
2024-12-12 23:05:32 +0100

    change background

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,14 @@ function App() {
     <>
       <div className="relative h-screen">
         <div className="absolute inset-0">
-          <div className="absolute top-0 -z-10 h-full w-full bg-white [&>div]:absolute [&>div]:bottom-auto [&>div]:left-auto [&>div]:right-0 [&>div]:top-0 [&>div]:h-[500px] [&>div]:w-[500px] [&>div]:-translate-x-[30%] [&>div]:translate-y-[20%] [&>div]:rounded-full [&>div]:bg-[rgba(109,244,173,0.5)] [&>div]:opacity-50 [&>div]:blur-[80px]">
-            <div></div>
-          </div>
+          <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
         </div>
 
         <div className="relative z-10 flex h-full flex-col items-center justify-center px-4">
           <div className="max-w-3xl text-center">
-            <h1 className="mb-8 text-4xl font-bold tracking-tight sm:text-6xl lg:text-7xl text-slate-900">
+            <h1 className="mb-8 text-4xl font-bold tracking-tight sm:text-6xl lg:text-7xl text-slate-200">
               Drum
-              <span className="text-sky-900"> Machine</span>
+              <span className="text-sky-400"> Machine</span>
             </h1>
           </div>
         </div>
