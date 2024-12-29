48599154967d380718abb3a8d482f2f441465152
MohamedLamineF <fadimoh@hotmail.fr>
2024-12-29 22:56:32 +0100

    implement dark mode toggle with useDarkMode hook; update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import "./App.css";
+import { Footer } from "./components/Layaout/Footer";
+import { Header } from "./components/Layaout/Header";
+import { useDarkMode } from "./hooks/useDarkMode";
 
 function App() {
-  return (
-    <>
-      <div className="relative h-screen">
-        <div className="absolute inset-0">
-          <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-        </div>
+  const { isDark, setIsDark } = useDarkMode();
 
-        <div className="relative z-10 flex h-full flex-col items-center justify-center px-4">
-          <div className="max-w-3xl text-center">
-            <h1 className="mb-8 text-4xl font-bold tracking-tight sm:text-6xl lg:text-7xl text-slate-200">
-              Drum
-              <span className="text-sky-400"> Machine</span>
-            </h1>
-          </div>
-        </div>
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header isDark={isDark} onToggleTheme={() => setIsDark(!isDark)} />
+      <div className="flex-1 relative">
+        <div
+          className={`absolute inset-0 -z-10 h-full w-full items-center ${
+            isDark
+              ? "[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"
+              : "[background:radial-gradient(125%_125%_at_50%_10%,#f5f5f5_40%,#63e_100%)]"
+          }`}
+        ></div>
+        <main className="container mx-auto px-4 py-8 flex items-center justify-center"></main>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 }
 
